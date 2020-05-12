a4c99a1b8d4c084fd416425a3cec6a07b7d2c4be
lupus117 <lupus@DESKTOP-CVB419K.localdomain>
2020-05-12 14:12:09 +0300

    add colours

--- srv1.js
+++ srv1.js
@@ -1,12 +1,27 @@
 const ws = require('ws')
 const rolls = require('./rolls')
 const wss = new ws.Server({ port: 8080 })
-//create connection array
+//the var holding all the connections
 Connections = [];
-var ConnectionNames = []
+//the var holding the nicknames
+var ConnectionNames = [];
 var stdin = process.openStdin();
+let nameChangeVar = "-name:";
 
-console.log('server initiated')
+//The conosle colours
+RedColor='\033[0;31m'
+GreenColor='\033[0;32m'
+BrownColor='\033[0;33m'
+BlueColor='\033[0;34m'
+PurpleColor='\033[0;35m'
+CyanColor='\033[0;36m'
+GrayColor='\033[0;37m'
+NoColor='\033[0m'
+Underscore='\033[4m'
+
+
+
+console.log("\033[31;1;4mServer initiated\033[0m")
 wss.on('connection', Server => {
  
  //Add the made collection to the array
@@ -14,20 +29,24 @@ wss.on('connection', Server => {
   //add event for recieving messages
   let n = Connections.length-1;
   ConnectionNames.push(n);
+
+
+  //ON connection message
   Connections[Connections.length-1].addEventListener('message', function(event){
-    console.log(`>[${n}]${ConnectionNames[n]}:${event.data}`)
     //add nickname option
-    let nameChangevar = "-name:";
-    if(event.data.substring(0,nameChangevar.length) == nameChangevar){
-      ConnectionNames[n] = event.data.substring(nameChangevar.length).trim();
-      console.log(`player ${n} is now  ${ConnectionNames[n]}`)
+    if(event.data.substring(0,nameChangeVar.length) == nameChangeVar){
+      ConnectionNames[n] = event.data.substring(nameChangeVar.length).trim();
+      console.log(`${Underscore}player ${RedColor}${Underscore}[${n}]${NoColor}${Underscore} is now  ${RedColor}${Underscore}${ConnectionNames[n]}${NoColor}`)
     }
     //send each connection the message
+    let message = rolls.searchAndRollDice(event.data);
+    console.log(`>${RedColor}[${n}]${NoColor}${ConnectionNames[n]}:${message}`)
     sendToAll(`player ${ConnectionNames[n]}:  ${rolls.searchAndRollDice(event.data)}`);
     
   });
+  //ON connection close
   Connections[Connections.length-1].on('close', event => {
-    console.log(`Connection closed by [${n}]${ConnectionNames[n]}`)
+    console.log(`${Underscore}Connection closed by ${RedColor}${Underscore}[${n}]${NoColor}${Underscore}$${ConnectionNames[n]}${NoColor}`)
 
     sendToAll(`> ${ConnectionNames[n]} left the server`);
   //  Connections.remove(n);
@@ -37,9 +56,9 @@ wss.on('connection', Server => {
   });
   
  
-  Server.send('You are now connected to the server\nuse "-name: " to change name')
+  Server.send(`You are now connected to the server\nuse "${nameChangeVar} " to change name`)
 
-  console.log(`Number of users: ${Connections.length}`)
+  console.log(`${Underscore}Number of users: ${GreenColor}${Underscore}${Connections.length}${NoColor}`)
 
 });
 
@@ -54,6 +73,7 @@ stdin.addListener("data", function(d) {
 function sendToAll(string){
   Connections.forEach(connection => {
     if(connection.OPEN){
+      
       connection.send(string)
     }
   });
